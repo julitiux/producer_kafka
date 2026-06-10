8b0294922f68b7945efad00f3abc6d16577d7194
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-10 17:55:19 -0600

    implement method sendMessage using the kafkaTemplate

--- src/main/java/com/kafka/wikimedia/producer_kafka/producer/WikimediaProducer.java
+++ src/main/java/com/kafka/wikimedia/producer_kafka/producer/WikimediaProducer.java
@@ -1,4 +1,20 @@
 package com.kafka.wikimedia.producer_kafka.producer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class WikimediaProducer {
+
+  private final KafkaTemplate<String, String> kafkaTemplate;
+
+  public WikimediaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  public void sendMessage(String message) {
+    kafkaTemplate.send("wikimedia-stream", message);
+  }
 }
