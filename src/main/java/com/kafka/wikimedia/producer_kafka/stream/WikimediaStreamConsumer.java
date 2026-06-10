e215d323d32283d56eacaf06d9acbbcb1fe4fc7e
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-10 17:58:12 -0600

    inject the bean WebClient

--- src/main/java/com/kafka/wikimedia/producer_kafka/stream/WikimediaStreamConsumer.java
+++ src/main/java/com/kafka/wikimedia/producer_kafka/stream/WikimediaStreamConsumer.java
@@ -1,4 +1,16 @@
 package com.kafka.wikimedia.producer_kafka.stream;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+@Service
 public class WikimediaStreamConsumer {
+
+  private final WebClient webClient;
+
+  public WikimediaStreamConsumer(WebClient webClient) {
+    this.webClient = webClient;
+  }
 }
