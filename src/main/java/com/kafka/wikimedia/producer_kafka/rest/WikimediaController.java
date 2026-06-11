1d7a325adca0ff964f14e043543b86a47c6b4a8f
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-10 18:21:04 -0600

    implement the method startPublishing using the bean WikimediaStreamConsumer

--- src/main/java/com/kafka/wikimedia/producer_kafka/rest/WikimediaController.java
+++ src/main/java/com/kafka/wikimedia/producer_kafka/rest/WikimediaController.java
@@ -1,4 +1,22 @@
 package com.kafka.wikimedia.producer_kafka.rest;
 
+import com.kafka.wikimedia.producer_kafka.stream.WikimediaStreamConsumer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/wikimedia")
 public class WikimediaController {
+
+  private final WikimediaStreamConsumer wikimediaStreamConsumer;
+
+  public WikimediaController(WikimediaStreamConsumer wikimediaStreamConsumer) {
+    this.wikimediaStreamConsumer = wikimediaStreamConsumer;
+  }
+
+  @GetMapping
+  public void startPublishing(){
+    wikimediaStreamConsumer.consumeStreamAndPublish();
+  }
 }
