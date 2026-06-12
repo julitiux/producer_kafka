dddb6abc58122f20f3db9cb2a5cc5dcecfec8216
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-11 19:40:24 -0600

    add bean to consume the data and send it to kafka

--- src/main/java/com/kafka/wikimedia/producer_kafka/stream/WikimediaStreamConsumer.java
+++ src/main/java/com/kafka/wikimedia/producer_kafka/stream/WikimediaStreamConsumer.java
@@ -1,5 +1,6 @@
 package com.kafka.wikimedia.producer_kafka.stream;
 
+import com.kafka.wikimedia.producer_kafka.producer.WikimediaProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,11 +10,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WikimediaStreamConsumer {
 
   private final WebClient webClient;
+  private final WikimediaProducer wikimediaProducer;
 
-  public WikimediaStreamConsumer(WebClient.Builder webClientBuilder) {
+  public WikimediaStreamConsumer(WebClient.Builder webClientBuilder, WikimediaProducer wikimediaProducer) {
     this.webClient = webClientBuilder
       .baseUrl("https://stream.wikimedia.org/v2")
       .build();
+    this.wikimediaProducer = wikimediaProducer;
   }
 
   public void consumeStreamAndPublish() {
@@ -22,6 +25,6 @@ public class WikimediaStreamConsumer {
       .uri("/stream/recentchange")
       .retrieve()
       .bodyToFlux(String.class)
-      .subscribe(log::info);
+      .subscribe(wikimediaProducer::sendMessage);
   }
 }
