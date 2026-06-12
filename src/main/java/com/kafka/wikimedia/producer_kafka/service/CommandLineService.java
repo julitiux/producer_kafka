031ddd2ac40054aed3c99565b2c92b5de7f2cd03
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-06-11 19:37:53 -0600

    implement method run from CommandLineRunner

--- src/main/java/com/kafka/wikimedia/producer_kafka/service/CommandLineService.java
+++ src/main/java/com/kafka/wikimedia/producer_kafka/service/CommandLineService.java
@@ -1,4 +1,24 @@
 package com.kafka.wikimedia.producer_kafka.service;
 
-public class CommandLineService {
+import com.kafka.wikimedia.producer_kafka.stream.WikimediaStreamConsumer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CommandLineService implements CommandLineRunner {
+
+  private final WikimediaStreamConsumer wikimediaStreamConsumer;
+
+  public CommandLineService(WikimediaStreamConsumer wikimediaStreamConsumer) {
+    this.wikimediaStreamConsumer = wikimediaStreamConsumer;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+
+    log.info("Wikimedia stream consumer started");
+    wikimediaStreamConsumer.consumeStreamAndPublish();
+  }
 }
